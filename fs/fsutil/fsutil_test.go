791a395f3632e64a52ceb9cfe97c2ef096d5def3
Rafal Jeczalik <rjeczalik@gmail.com>
2014-08-10 20:42:21 +0200

    fs/fsutil: Rewrite TestReaddirpaths

--- fs/fsutil/fsutil_test.go
+++ fs/fsutil/fsutil_test.go
@@ -54,28 +54,81 @@ func TestReadpaths(t *testing.T) {
 }
 
 func TestReaddirpaths(t *testing.T) {
-	cases := map[string][]string{
-		filepath.FromSlash("/data/github.com/user/example"): {
-			"assets",
-			"dir",
+	cases := [...]struct {
+		c    Control
+		dirs map[string][]string
+	}{
+		0: {
+			Control{FS: trees[0]},
+			map[string][]string{
+				filepath.FromSlash("/data/github.com/user/example"): {
+					"assets",
+					"dir",
+				},
+				filepath.FromSlash("/src/github.com/user/example"): {
+					"dir",
+				},
+			},
 		},
-		filepath.FromSlash("/src/github.com/user/example"): {
-			"dir",
+		1: {
+			Control{FS: trees[0], Hidden: true},
+			map[string][]string{
+				filepath.FromSlash("/data/github.com/user/example"): {
+					"assets",
+					"dir",
+					".git",
+				},
+				filepath.FromSlash("/src/github.com/user/example"): {
+					"dir",
+					".git",
+				},
+			},
+		},
+		2: {
+			Control{FS: trees[1]},
+			map[string][]string{
+				filepath.FromSlash("/"): {
+					"data",
+					"src",
+				},
+				filepath.FromSlash("/data/github.com/user/example"): {
+					"dir",
+					"first",
+					"second",
+				},
+				filepath.FromSlash("/src"): {
+					"github.com",
+				},
+			},
+		},
+		3: {
+			Control{FS: trees[2]},
+			map[string][]string{
+				filepath.FromSlash("/"): {
+					"schema",
+					"src",
+				},
+				filepath.FromSlash("/schema/licstat/schema"): {
+					"databasequery",
+					"generalinfo",
+					"license",
+					"monitorconf",
+				},
+				filepath.FromSlash("/src/licstat/schema"): {
+					"tmp",
+				},
+			},
 		},
 	}
-	c := Control{FS: trees[0]}
-	for dir, cas := range cases {
-		for _, b := range [...]bool{false, true} {
-			if c.Hidden = b; b {
-				cas = append(cas, ".git")
-			}
-			names := c.Readdirpaths(dir)
-			if names == nil {
-				t.Errorf("want names!=nil (dir=%q,hidden=%v)", dir, b)
+	for i, cas := range cases {
+		for dir, v := range cas.dirs {
+			paths := cas.c.Readdirpaths(dir)
+			if paths == nil {
+				t.Errorf("want paths!=nil (i=%d, dir=%s)", i, dir)
 				continue
 			}
-			if !equal(names, cas) {
-				t.Errorf("want names=%v; got %v (dir=%q,hidden=%v)", cas, names, dir, b)
+			if !equal(paths, v) {
+				t.Errorf("want paths=%v; got %v (i=%d, dir=%s)", v, paths, i, dir)
 			}
 		}
 	}
