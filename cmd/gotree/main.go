rjeczalik/cmd

commit f6b048fe19afac289ca9aa5ef3955b6eb504c6e4

cmd/gotree: Provide custom flag.Usage function

cmd/gotree/main.go
@@ -69,13 +69,18 @@ var (
 	varname string
 )
 
+var flags = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
 func init() {
-	flag.BoolVar(&all, "a", false, "")
-	flag.BoolVar(&dir, "d", false, "")
-	flag.IntVar(&lvl, "L", 0, "")
-	flag.IntVar(&gowidth, "go", 0, "")
-	flag.StringVar(&varname, "var", "", "")
-	flag.Parse()
+	flags.Usage = func() {
+		fmt.Fprintln(os.Stderr, usage)
+	}
+	flags.BoolVar(&all, "a", false, "")
+	flags.BoolVar(&dir, "d", false, "")
+	flags.IntVar(&lvl, "L", 0, "")
+	flags.IntVar(&gowidth, "go", 0, "")
+	flags.StringVar(&varname, "var", "", "")
+	flags.Parse(os.Args[1:])
 }
 
 func die(v interface{}) {

commit c7c649ce191f3cdca03b22c20a7f5bcd360ada29

cmd/gotree: Fix for flag parsing broken by f6b048fe19

cmd/gotree/main.go
@@ -129,8 +129,8 @@ func main() {
 		spy       = memfs.New()
 		printroot = true
 	)
-	if len(flag.Args()) == 1 {
-		root = flag.Args()[0]
+	if len(flags.Args()) == 1 {
+		root = flags.Args()[0]
 	}
 	if root == "." {
 		root, _ = os.Getwd()